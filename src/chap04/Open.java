04bb924c44cabc9b7c6725646cc88c5353d0e84f
chansik <chatter0502@gmail.com>
2016-12-29 13:45:12 +0900

    added the open problem (complete)

--- src/chap04/Open.java
+++ src/chap04/Open.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * Created by AIServer on 2016-12-26.
  */
 public class Open {
-    class Player {
+    static class Player {
         String name;
         String word;
         Player() {
@@ -30,23 +30,23 @@ public class Open {
             return name;
         }
         boolean succeed(String last) {
-            return last.charAt(last.length()-1) == word.charAt(0);
+            return last.charAt(last.length() - 1) == word.charAt(0);
         }
     }
     public static void main(String [] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("게임에 참가한느 인원은 몇명입니까>>");
+        System.out.print("게임에 참가하는 인원은 몇명입니까>>");
         int n = in.nextInt();
         Player [] players = new Player[n];
         for (int i = 0; i < players.length; ++i) {
             System.out.print("참가자의 이름을 입력하세요>>");
             String name = in.next();
-//            players[i].set(name); // 여기에서 에러 => 널포인터예외 발생.. 왜지
-            players[i].set(name);
+            players[i] = new Player(name);
         }
         String last = "아버지";
         System.out.println("시작하는 단어는 " + last + "입니다.");
         for (int i = 0;; ++i, i %= players.length) {
+            System.out.print(players[i % n].getname() + ">>");
             players[i].sayWord(in);
             if (!players[i].succeed(last)) {
                 System.out.println(players[i].getname() + "이 졌습니다.");
